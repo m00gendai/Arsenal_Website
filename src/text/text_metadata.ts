6ca60720ae3eeab01a68e2f3df5bc2439b65b261
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-03-07 08:40:11 +0000

    New metadata textfile

---
+++ src/text/text_metadata.ts
@@ -0,0 +1,25 @@
+import type { SimpleTranslation } from "../types/types_global";
+
+export const metadata_title: SimpleTranslation = {
+    de: `Arsenal - Die Schweizer App für Waffensammler`,
+    en: `Arsenal - The Swiss App for Gun Collectors`,
+    fr: `Arsenal - L’application suisse pour les collectionneurs d’armes`
+}
+
+export const metadata_description: SimpleTranslation = {
+    de: `Verwalten Sie Ihre Waffensammlung, den Munitionsbestand, Ihr Zubehör, Waffenbestandteile und Fachliteratur mit dieser bequemen App!`,
+    en: `Manage your Gun Collection, Ammo Stock and accessories with this convenient app!`,
+    fr: `Gérez votre collection d’armes à feu, vos munitions, vos accessoires, vos pièces d’armes et votre littérature sur les armes à feu avec cette application pratique!`
+}
+
+export const metadata_appName: SimpleTranslation = {
+    de: `Arsenal Waffen & Sammlung`,
+    en: `Arsenal Gun Collection`,
+    fr: `Arsenal Collection d'Armes`
+}
+
+export const metadata_featureImageAlt: SimpleTranslation = {
+    de: `Vorstellungsbild von einigen Screenshots der App in Aktion`,
+    en: `Feature Image of some Screenshots of the App in action`,
+    fr: `Image principale de quelques captures d’écran de l’application en action`
+}
