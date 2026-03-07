1f82a18c938cc1787b08c265f9db51d4add0a2fc
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-03-07 09:04:06 +0000

    Aded sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-  locales: ["de", "en", "fr", "it"],
-  defaultLocale: "en",
-  routing: {
+    locales: ["de", "en", "fr"],
+    defaultLocale: "en",
+    routing: {
       prefixDefaultLocale: true
-  }
-},
-
-  integrations: [react()]
+    }
+  },
+  integrations: [
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://example.com/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+          de: 'de_DE',
+          fr: 'fr-FR',
+        },
+      },
+    }),
+  ]
 });
