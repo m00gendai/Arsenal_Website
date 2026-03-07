159bf5b15b93dfcb34994192bcd2f5c1d280a75a
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-03-07 08:37:18 +0000

    Removed locale

--- src/components/Appstores.tsx
+++ src/components/Appstores.tsx
@@ -6,19 +6,13 @@ interface Props{
 
 export default function Appstores({language}:Props){
 
-    const locales = {
-        de: "ch",
-        en: "us",
-        fr: "fr"
-    }
-
     return (
         <section className="content">
             <div className="badgeContainer">
                 <a href={`https://play.google.com/store/apps/details?id=com.m00gendai.arsenal&pli=1&hl=${language}`} target="_blank">
                     <img src={`../src/assets/android_${language}.svg`} width="100%" alt="Google Playstore Download Link" />
                 </a>
-                <a href={`https://apps.apple.com/${locales[language]}/app/arsenal-armamentarium/id6670214648`} target="_blank">
+                <a href={`https://apps.apple.com/app/arsenal-armamentarium/id6670214648`} target="_blank">
                     <img src={`../src/assets/ios_${language}.svg`} width="100%" alt="AppleAppstore Download Link" />
                 </a>
             </div>
