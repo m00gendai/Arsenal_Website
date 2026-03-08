13b076337c6476522e76150c02048f5f977db1c9
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-03-08 12:13:26 +0000

    added it and ch

--- src/types/types_global.ts
+++ src/types/types_global.ts
@@ -4,10 +4,14 @@ export interface SimpleTranslation{
     de: string
     en: string
     fr: string
+    it?: string
+    ch?: string
 }
 
 export interface SimpleTranslation_StringArray{
     de: (string | string[])[]
     en: (string | string[])[]
     fr: (string | string[])[]
+    it?: (string | string[])[]
+    ch?: (string | string[])[]
 }
