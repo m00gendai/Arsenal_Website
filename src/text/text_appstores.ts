70ee67810a96568c5e1ae89ff33564c473545a9e
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-03-09 14:13:00 +0000

    Text for Appstores component

---
+++ src/text/text_appstores.ts
@@ -0,0 +1,7 @@
+import type { SimpleTranslation } from "../types/types_global";
+
+export const starsText: SimpleTranslation = {
+    de: `{{{A}}}/5 Sterne bei {{{B}}} Bewertungen (iOS)`,
+    en: `{{{A}}}/5 stars from {{{B}}} ratings (iOS)`,
+    fr: `{{{A}}}/5 étoiles sur {{{B}}} avis (iOS)`,
+}
