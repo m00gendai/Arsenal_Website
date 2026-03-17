781ed68a0ca8b9d7b892e2c9ec69e2c9812118a7
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-03-17 16:46:27 +0000

    Dedicated module css for accordions

--- src/components/FAQ/faq_accordions.tsx
+++ src/components/FAQ/faq_accordions.tsx
@@ -1,5 +1,6 @@
 import { faq } from "../../text/text_faq"
 import type { Language } from "../../types/types_global"
+import s from "../../styles/accordion.module.css"
 
 interface Props{
     language: Language
@@ -7,25 +8,25 @@ interface Props{
 
 export default function Faq_accordions({language}:Props){
     return(
-        <div className="accordionContainer">
+        <div className={s.container}>
             {faq.map((entry, index) => {
                 return(
-                    <details key={`entry_${index}`}>
-                        <summary>
+                    <details className={s.details} key={`entry_${index}`}>
+                        <summary className={s.summary}>
                             {entry.question[language]}
                         </summary>
-                        <div className="accordionContent">
+                        <div className={s.content}>
                             {entry.answer[language].map((answer, indx) => {
                                 if(Array.isArray(answer)){
                                     return(
-                                        <ul key={`faq_ul_${indx}`}>
+                                        <ul className={s.list} key={`faq_ul_${indx}`}>
                                             {answer.map((listItem, idx) => {
                                                 return <li key={`faq_listItem_${idx}`} dangerouslySetInnerHTML={{__html: listItem}}></li>
                                             })}
                                         </ul>
                                     )
                                 } else {
-                                    return <p style={{margin: "1rem"}} key={`faq_answer_${indx}`} dangerouslySetInnerHTML={{__html: answer}}></p>
+                                    return <p className={s.text} key={`faq_answer_${indx}`} dangerouslySetInnerHTML={{__html: answer}}></p>
                                 }                   
                             })}
                         </div>
